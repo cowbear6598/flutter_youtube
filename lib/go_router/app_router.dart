a34e0ee1bed882ca8a70c9acbf308ff060c4b1f6
CptSoap <cowbear6598@gmail.com>
2023-10-02 23:33:39 +0800

    feat: add multiple compare path print

--- lib/go_router/app_router.dart
+++ lib/go_router/app_router.dart
@@ -2,5 +2,13 @@ import 'package:flutter_youtube/go_router/go_router_builder.dart';
 import 'package:go_router/go_router.dart';
 
 final router = GoRouter(
+  redirect: (context, state) {
+    print("uri: ${state.uri}");
+    print("uri's path: ${state.uri.path}");
+    print("matched: ${state.matchedLocation}");
+    print("full: ${state.fullPath}");
+
+    return null;
+  },
   routes: $appRoutes,
 );
